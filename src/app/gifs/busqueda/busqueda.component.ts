d6bbce24369c45e6eaab7e6666a72b239e7eeb39
Agustin Biagi <agustin.biagi@gmail.com>
2021-09-04 21:36:21 -0300

    .

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { GifsService } from '../services/gifs.service';
+
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
@@ -10,11 +12,13 @@ export class BusquedaComponent {
   
 	@ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+	constructor (private gifsServices:GifsService){}
+
 	buscar(){
-		
+
 		const valor= this.txtBuscar.nativeElement.value;
 		
-		console.log(valor);
+		this.gifsServices.buscarGifs(valor);
 		this.txtBuscar.nativeElement.value="";
 		
 		
